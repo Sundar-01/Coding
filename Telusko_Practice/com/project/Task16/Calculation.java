3647f8108787a76ec1a8769ad2ba295f8bb624e4
sundar <mailtosundars96@gmail.com>
2025-05-18 07:13:17 +0530

    Task16

---
+++ Telusko_Practice/com/project/Task16/Calculation.java
@@ -0,0 +1,7 @@
+package com.project.Task16;
+
+class Calculation {
+    public int dividor(int in1, int in2){
+        return in1/in2;
+    }
+}
--- Telusko_Practice/com/project/Task16/Main.java
+++ Telusko_Practice/com/project/Task16/Main.java
@@ -1,5 +1,22 @@
 package com.project.Task16;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        String[] names = {"Yaazhan", "Sundar", "Aishwarya"};
+        int in1 = 5, in2 = 0;
+        try{
+            //Calculation obj = new Calculation();
+            //int result = obj.dividor(5, 0);
+            int result = in1/in2;
+            System.out.println("Result of Calculation is : " + result);
+            String result1 = names[3];
+            System.out.println("Result of Array is : " + result1);
+        }catch (ArithmeticException a){
+            System.out.println("Thrown an Arithmetic Exception. Please check the value.");
+        }catch (ArrayIndexOutOfBoundsException o){
+            System.out.println("Invalid array index!");
+        }finally{
+            System.out.println("End of program. Resources closed.");
+        }
+    }
 }
