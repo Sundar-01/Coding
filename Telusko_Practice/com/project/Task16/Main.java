8e36371da58c1e021f82eed9d57ba8894af6257f
sundar <mailtosundars96@gmail.com>
2025-05-18 07:44:04 +0530

    Modified old Task16

--- Telusko_Practice/com/project/Task16/Main.java
+++ Telusko_Practice/com/project/Task16/Main.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         String[] names = {"Yaazhan", "Sundar", "Aishwarya"};
         int in1 = 5, in2 = 0;
+        //Calculation obj = new Calculation();
         try{
-            //Calculation obj = new Calculation();
-            //int result = obj.dividor(5, 0);
-            int result = in1/in2;
-            System.out.println("Result of Calculation is : " + result);
-            String result1 = names[3];
-            System.out.println("Result of Array is : " + result1);
+            //int result = obj.dividor(in1, in2);
+            System.out.println("Result of Calculation is : " + in1/in2);
+            
         }catch (ArithmeticException a){
             System.out.println("Thrown an Arithmetic Exception. Please check the value.");
+        }
+        try{
+            String result1 = names[3];
+            System.out.println("Result of Array is : " + result1);
         }catch (ArrayIndexOutOfBoundsException o){
             System.out.println("Invalid array index!");
         }finally{
